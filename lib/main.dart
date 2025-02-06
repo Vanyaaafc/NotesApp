ea23e245e1c4b48ff5bccb84b6717fa68aac1f06
Ivan Syvak <Ivan.Syvak@fivesysdev.com>
2025-02-06 15:23:43 +0200

    add AutoRoute navigation

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,8 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:notes_app/features/auth_screen/domain/auth_bloc.dart';
-import 'package:notes_app/features/auth_screen/presentation/auth_screen.dart';
 import 'package:notes_app/features/sign_up/domain/sign_up_bloc.dart';
+import 'package:notes_app/routing/app_router.dart';
 
 import 'di.dart';
 import 'firebase_options.dart';
@@ -17,9 +17,15 @@ void main() async {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  final _appRouter = AppRouter();
   @override
   Widget build(BuildContext context) {
     return MultiBlocProvider(
@@ -27,13 +33,13 @@ class MyApp extends StatelessWidget {
         BlocProvider.value(value: g.get<AuthBloc>()..add(AuthInitialEvent())),
         BlocProvider.value(value: g.get<SignUpBloc>()..add(SignUpInitialEvent()))
       ],
-      child: MaterialApp(
+      child: MaterialApp.router(
         debugShowCheckedModeBanner: false,
         theme: ThemeData(
           colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
           useMaterial3: true,
         ),
-        home: const AuthScreen(),
+        routerConfig: _appRouter.config(),
       ),
     );
   }
